f3a48997c63358bd735cabc84d1018ff498c371c
Roman Trippel <contac@romantrippel.com>
2026-03-09 20:55:30 +0100

    Initial project setup

---
+++ src/main/java/com/romantrippel/linkedin_job_parser/LinkedinJobParserApplication.java
@@ -0,0 +1,13 @@
+package com.romantrippel.linkedin_job_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinJobParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinJobParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romantrippel/linkedin_job_parser/LinkedinJobParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romantrippel.linkedin_job_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinJobParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
